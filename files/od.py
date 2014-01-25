66831065a131ba52bc371a25a9f0fd844b482956
Gary <garyb.davies@btinternet.com>
2014-01-25 17:41:58 +0100

    Just adding .gitignore files and other cleanups

---
+++ files/od.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+
+class OutputDir(object):
+    """Create an output directory object e.g. output_dir = OutputDir('path-to-file')"""
+    def __init__(self,output_dir):
+        self.dir = output_dir
+
+    def path(self,string=None):
+        """Returns the full path of the output directory"""
+        full_path = os.path.abspath(self.dir)
+        if string is not None:
+            return os.path.join(full_path,string)
+        else:
+            return full_path
+    
+    def list(self,prefix=None,file_ext=None):
+        """Returns a list of the contents of the output directory"""
+        if prefix is None and file_ext is None:
+            return os.listdir(self.path())
+        elif prefix is not None and file_ext is None:
+            return [file for file in self.list() if prefix in file.split('_')[0]]
+        elif prefix is None and file_ext is not None:
+            return [file for file in self.list() if (os.path.splitext(file)[1] == file_ext)]
+        else: 
+            return [file for file in self.list() if (prefix in file.split('_')[0] and os.path.splitext(file)[1] == file_ext)]
+            
+    def filter(self,prefix,file_ext):
+        """Returns a list of files which match prefix and suffix"""
+        return [file for file in self.list if (prefix in file.split('_')[0] and os.path.splitext(file)[1] == file_ext)]
+
+    def filter_ext(self,file_ext):
+        """Returns a list of files which match the file extension"""
+        return [file for file in self.list if (os.path.splitext(file)[1] == file_ext)]
