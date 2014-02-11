f019d36de6da5888f1578ab6278013529e5ea852
Gary <garyb.davies@btinternet.com>
2014-02-11 01:21:31 +0000

    Made the list function return sorted lists

--- files.py
+++ files.py
@@ -19,13 +19,14 @@ class OutputDir(object):
     def list(self,prefix=None,file_ext=None):
         """Returns a list of the contents of the output directory"""
         if prefix is None and file_ext is None:
-            return os.listdir(self.path())
+            L = os.listdir(self.path())
         elif prefix is not None and file_ext is None:
-            return [file for file in self.list() if prefix in file.split('_')[0]]
+            L = [file for file in self.list() if prefix in file.split('_')[0]]
         elif prefix is None and file_ext is not None:
-            return [file for file in self.list() if (os.path.splitext(file)[1] == file_ext)]
+            L = [file for file in self.list() if (os.path.splitext(file)[1] == file_ext)]
         else: 
-            return [file for file in self.list() if (prefix in file.split('_')[0] and os.path.splitext(file)[1] == file_ext)]
+            L = [file for file in self.list() if (prefix in file.split('_')[0] and os.path.splitext(file)[1] == file_ext)]
+        return sorted(L)
 
 class MDFile(object):
     """Create an MD file object e.g. bla = MDFile('md-filename-here')"""
